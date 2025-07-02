6f0afea99e01d1e1b1923d24ecbfcf5d62fa6e64
= <=>
2025-07-02 21:06:51 +0530

    Deploy project on Render

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENV != "production"){
-    require("dotenv").config();
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
 }
 
 const express = require("express");
@@ -11,19 +11,19 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/expressError.js");
 const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
+const port = process.env.PORT || 8080;
 
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
@@ -32,36 +32,44 @@ app.set("views engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL =
+  process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wanderlust";
 
-main().then(() => {
+main()
+  .then(() => {
     console.log("connection success");
-}).catch((e) => {
+  })
+  .catch((e) => {
     console.log(e);
-})
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
 
-const store = new MongoStore({
-    url : MONGO_URL,
-    secret : process.env.SECRET,
-    touchAfter : 24 * 3600
+const store = MongoStore.create({
+  mongoUrl: MONGO_URL,
+  dbName: "wanderlust",
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
 });
 
 const sessionOptions = {
-    store,
-    secret : process.env.SECRET,
-    resave : false,
-    saveUninitialized : true,
-    cookie : {
-        expires : Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge : 7 * 24 * 60 * 60 * 1000,
-        httpOnly : true,
-    }
-}
-
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -73,27 +81,27 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res, next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
-    next();
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
 });
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-app.listen(8080, () => {
-    console.log("app is listening on port 8080");
+app.listen(port, () => {
+  console.log("app is listening on port 8080");
 });
 
-app.all("*", (req,res,next) => {
-    next(new ExpressError(404, "Page Not Found :("));
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found :("));
+});
 
-app.use((err, req,res, next) => {
-    let {statusCode=500 , message= "Something Went Wrong"} = err;
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something Went Wrong" } = err;
 
-    res.status(statusCode).render("error.ejs", {message});
-}) 
+  res.status(statusCode).render("error.ejs", { message });
+});
